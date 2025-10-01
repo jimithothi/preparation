c4441452ac2a8ddb8524c03fac91a62f64d9c881
Jimit <jimithothi@gmail.com>
2025-10-01 15:58:45 +0530

    Refactor MongoDB connection logic for improved readability and consistency

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -3,22 +3,22 @@ import mongoose from "mongoose";
 const connection = {};
 
 async function connect() {
-    if (connection.isConnected) {
-        console.log("Already connected to MongoDB");
-        return;
-    }
-    if (!process.env.MONGODB_URI) {
-        throw new Error("MONGODB_URI is not defined in environment variables");
-    }
-    try {
-        const db = await mongoose.connect(process.env.MONGODB_URI);
-        console.log("Connected to MongoDB");
-        connection.isConnected = db.connections[0].readyState;
-    } catch (error) {
-        console.error("MongoDB connection error:", error);
-        connection.isConnected = 0;
-        process.exit(1);
-    }
+  if (connection.isConnected) {
+    console.log("Already connected to MongoDB");
+    return;
+  }
+  if (!process.env.MONGODB_URI) {
+    throw new Error("MONGODB_URI is not defined in environment variables");
+  }
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB");
+    connection.isConnected = db.connections[0].readyState;
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    connection.isConnected = 0;
+    process.exit(1);
+  }
 }
 
 export default connect;
